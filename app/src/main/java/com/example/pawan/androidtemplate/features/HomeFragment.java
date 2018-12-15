04181cb005288fdbcf69b2fb69515b7eeaa74fe0
pawan6186 <pawan6186.sd@gmail.com>
2018-12-16 00:33:15 +0530

    Changes to load fact data in recycler view and implemented pull to refresh.

--- app/src/main/java/com/example/pawan/androidtemplate/features/HomeFragment.java
+++ app/src/main/java/com/example/pawan/androidtemplate/features/HomeFragment.java
@@ -5,6 +5,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +21,9 @@ import timber.log.Timber;
 
 public class HomeFragment extends BaseFragment<HomeViewModel> {
     private FragmentHomeBinding mFragmentHomeBinding;
-    public static HomeFragment newInstance(){
+    private FactsAdapter mFactAdapter;
+
+    public static HomeFragment newInstance() {
         return new HomeFragment();
     }
 
@@ -34,14 +37,35 @@ public class HomeFragment extends BaseFragment<HomeViewModel> {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mFragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home,container,false);
+        mFragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        initViews();
         mViewModel.getFacts().observe(getViewLifecycleOwner(), new Observer<List<Fact>>() {
             @Override
             public void onChanged(@Nullable List<Fact> facts) {
-                Timber.d(facts.toString());
+                if (facts != null) {
+                    mFactAdapter.updateData(facts);
+                }
             }
         });
         return mFragmentHomeBinding.getRoot();
+    }
+
+    private void initViews() {
+        mFactAdapter = new FactsAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(mFragmentHomeBinding.recyclerView.getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mFragmentHomeBinding.recyclerView.setLayoutManager(layoutManager);
+        mFragmentHomeBinding.recyclerView.setHasFixedSize(true);
+        mFragmentHomeBinding.recyclerView.setAdapter(mFactAdapter);
+        mFragmentHomeBinding.swipeContainer.setOnRefreshListener(() -> mViewModel.getFacts().observe(getViewLifecycleOwner(), new Observer<List<Fact>>() {
+            @Override
+            public void onChanged(@Nullable List<Fact> facts) {
+                if (facts != null) {
+                    mFactAdapter.updateData(facts);
+                }
+                mFragmentHomeBinding.swipeContainer.setRefreshing(false);
+            }
+        }));
     }
 }
 
